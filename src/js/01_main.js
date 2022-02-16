ab4974fb1bd9e98eb21b7982d4f0bda2fae5bbdd
Maria Pensativa <mariya.pensativa@gmail.com>
2022-02-16 23:40:45 +0200

    Add new pages

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -158,3 +158,54 @@ const calc = document.querySelector('.calc');
 if (calc) {
   fileInputChangeBg();
 }
+
+//Filters
+(function() {
+  const parent = document.querySelector('.range-slider');
+
+  if (!parent) {
+      return;
+  }
+
+  const rangeS = parent.querySelectorAll('input[type="range"]'),
+        numberS = parent.querySelectorAll('input[type="number"]');
+
+  rangeS.forEach((el) => {
+      el.oninput = () => {
+          let slide1 = parseFloat(rangeS[0].value),
+              slide2 = parseFloat(rangeS[1].value);
+
+          if (slide1 > slide2) {
+              [slide1, slide2] = [slide2, slide1];
+          }
+
+          numberS[0].value = slide1;
+          numberS[1].value = slide2;
+      }
+  });
+
+  numberS.forEach((el) => {
+      el.oninput = () => {
+          let number1 = parseFloat(numberS[0].value),
+              number2 = parseFloat(numberS[1].value);
+
+          if (number1 > number2) {
+              let tmp = number1;
+              numberS[0].value = number2;
+              numberS[1].value = tmp;
+          }
+
+          rangeS[0].value = number1;
+          rangeS[1].value = number2;
+      }
+  });
+})();
+
+(function() {
+  const filterToggle = document.querySelector('.filter__toggel');
+  const filterForm = document.querySelector('.filter__form');
+
+  filterToggle.onclick = function() {
+    filterForm.classList.toggle('open');
+  }
+})();
